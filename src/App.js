aa67c4da586eb25113770bb96e8ffa467c095ae2
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-26 10:35:24 +0530

    added componenets folder and components , added some routes in app.js for routing with links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './App.css'
+import Add from './components/Add'
+import Watched from "./components/Watched"
+import WatchList from "./components/WatchList"
+import {Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import "./lib/font-awesome/css/all.min.css";
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header/>
+    <Routes>
+      <Route exact path="/" element={<WatchList/>} />
+      <Route path="/watched" element={<Watched/>} />
+      <Route path="/add" element={<Add/>} />
+    </Routes>
+    </>
   )
 }
 
