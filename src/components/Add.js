a6b1560f326ba8e815210a607d7a319bc3f4e175
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-26 13:01:08 +0530

    added code for search movies using search api from TMDB and set some state alos installed axios

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
+    const [query, setQuery] = useState()
+    const [result, setResult] = useState([])
+    const key = "01c3da185b5eef24879699f5137b813f";
+
+    const queryHandler = (e) => {
+        setQuery(e.target.value)
+
+
+    }
+
+
+    return (
+        <div className="add-page">
+            <div className="container">
+                <div className="add-content">
+                    <div className="input-wrapper">
+                        <input type="text" value={query} placeholder='Search for movies' onChange={queryHandler} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Add
